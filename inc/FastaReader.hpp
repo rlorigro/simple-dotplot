d7b8b300430c1e5e702f0dde991e24d7ea5d3658
Ryan <rlorigro@ucsc.edu>
2021-07-06 14:59:38 -0700

    Remove hts dependency

--- inc/FastaReader.hpp
+++ inc/FastaReader.hpp
@@ -38,13 +38,10 @@ class FastaReader{
 public:
     /// Attributes ///
     path file_path;
-    path index_path;
     char header_symbol;
     string eof_placeholder_name;
     uint64_t line_index;
     bool end_of_file;
-    vector <FastaIndex> sequential_read_offsets;
-    unordered_map <string, size_t > read_indexes_by_name;
 
     /// Methods ///
     FastaReader(path file_path);
@@ -53,24 +50,8 @@ public:
     // Fetch header + sequence assuming read position precedes a header
     bool next_element(SequenceElement& element);
 
-    // Generate index if needed, otherwise load index
-    void index();
-
-    // Return a copy of the read indexes
-    void get_indexes_mapped_by_name(unordered_map <string, FastaIndex>& indexes);
-
-    // Set this reader's index-related attributes to match the donor FastaReader
-    void adopt_index(FastaReader& donor);
-
-    // Fetch a sequence from file, and generate index first if necessary
-    void get_sequence(SequenceElement& element, string& sequence_name);
-    void get_sequence(SequenceElement& element, string& sequence_name, uint64_t fasta_byte_index);
-
     // Create a copy of the appropriate sequence container for this reader
     SequenceElement generate_sequence_container();
-
-    // Use htslib to make an .fai
-    void build_fasta_index();
 
 private:
     /// Attributes ///
@@ -83,9 +64,6 @@ private:
     // Assuming the read position of the ifstream is at a sequence, parse the sequence line(s),
     // which precede the next header
     void read_next_sequence(SequenceElement& element);
-
-    // Read fai
-    void read_fasta_index();
 };
 
 
